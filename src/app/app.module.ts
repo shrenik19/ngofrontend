a9af808d08422ed01dbf554e94f6360115c09ff9
pratikshah98 <pratikshah012325@gmail.com>
2020-05-08 00:10:59 +0530

    loginsignupcomponentadded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgetpasswordComponent } from './forgetpassword/forgetpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgetpasswordComponent
   ],
   imports: [
     BrowserModule,
