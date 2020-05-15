1f90c58daa644742bc5e23b4d7f8d5cad86a6179
pratikshah98 <pratikshah012325@gmail.com>
2020-05-15 11:43:12 +0530

    registerpageaddded

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _route:Router,private _location:Location) { }
+
+  refresh():void{
+    this._route.navigateByUrl('/signup',{skipLocationChange:true}).then(()=>{
+        console.log(this._location.path());
+      this._route.navigate([decodeURI( this._location.path())]);
+    });
+  }
 
   ngOnInit(): void {
   }
